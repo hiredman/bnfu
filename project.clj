fcab2f9524c61fb4265747fd7384080a7452664b
Kevin Downey <redchin@gmail.com>
2013-08-04 12:03:18 -0700

    0.0.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.thelastcitadel/bnfu "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.thelastcitadel/bnfu "0.0.1"
+  :description "bnf parsers for clojure"
+  :url "http://github.com/hiredman/bnfu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
